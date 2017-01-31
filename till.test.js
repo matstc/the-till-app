6e8f0761b0fd8afe9052c3472357cbd0fe25b0ca
Matt Tanguay-Carel <matthieutc@gmail.com>
2017-01-31 09:59:50 +0100

    adds solution

--- till.test.js
+++ till.test.js
@@ -7,18 +7,21 @@ test('it calculates 20% tax', () => {
 });
 
 test('looks up the price of 1000 for a laptop', () => {
-  // ...
+  expect(till.lookupPrice('laptop')).toBe(1000);
 });
 
 test('it throws an error if item not found in inventory', () => {
-  // ...
+  expect(function(){
+    till.lookupPrice('toy')
+  }).toThrow()
 });
 
 test('it calculates total sum for a laptop and shoes', () => {
-  // ...
+  expect(till.calculateTotalSum(['laptop', 'shoes'])).toBe(1320);
 });
 
 test('it prints the receipt properly when buying a laptop and a shirt', () => {
   const receipt = till.generateReceipt(['laptop', 'shirt']);
-  // ...
+
+  expect(receipt).toEqual("Item #0: laptop (1000 + 200 = 1200)\nItem #1: shirt (30 + 6 = 36)\nTotal: 1236")
 });
